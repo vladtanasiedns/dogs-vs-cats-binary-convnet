895525e930503a86439d603f69474719c6811a68
Tanasie <tanasie.vlad@dandsltd.com>
2021-02-03 16:05:12 +0200

    Binary categorical convnet example

---
+++ history.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import json
+
+history = json.loads('./history.json')
+
+print(history)
+
+acc = history['acc']
+val_acc = history['val_acc']
+loss = history['loss']
+val_loss = history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
---
+++ model.py
@@ -0,0 +1,55 @@
+from keras import models
+from keras import layers
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import json
+
+model = models.Sequential()
+
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['accuracy'])
+
+train_dir = "./dvc-small/train"
+test_dir = "./dvc-small/test"
+
+train_datagen = ImageDataGenerator(rescale=1.0/255)
+test_datagen = ImageDataGenerator(rescale=1.0/255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+
+validation_generator = test_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=2000 // 20,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=1000 // 20
+)
+
+model.save("cats_dogs_small.h5")
+with open("cats_dogs.json", "w") as history_json:
+    json.dump(history.history, history_json)
